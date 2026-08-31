23818e42f2905b9961e10581632c07be26f15a86
ark3127 <aman.kawale47@gmail.com>
2026-08-31 12:59:47 +0530

    Refactor SecureStorageService to use instance storage

--- lib/services/secure_storage_service.dart
+++ lib/services/secure_storage_service.dart
@@ -1,28 +1,30 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorageService {
-  SecureStorageService._();
-
-  static const _storage = FlutterSecureStorage();
-
   static const _apiKeyKey = 'nvidia_api_key';
 
-  static Future<void> saveApiKey(String apiKey) async {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future<void> saveApiKey(String apiKey) async {
     await _storage.write(
       key: _apiKeyKey,
-      value: apiKey.trim(),
+      value: apiKey,
     );
   }
 
-  static Future<String?> getApiKey() async {
-    return _storage.read(key: _apiKeyKey);
+  Future<String?> getApiKey() async {
+    return await _storage.read(
+      key: _apiKeyKey,
+    );
   }
 
-  static Future<void> deleteApiKey() async {
-    await _storage.delete(key: _apiKeyKey);
+  Future<void> deleteApiKey() async {
+    await _storage.delete(
+      key: _apiKeyKey,
+    );
   }
 
-  static Future<bool> hasApiKey() async {
+  Future<bool> hasApiKey() async {
     final apiKey = await getApiKey();
     return apiKey != null && apiKey.isNotEmpty;
   }
