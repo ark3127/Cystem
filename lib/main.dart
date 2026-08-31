c520b6ca842bc0388e0b1079671ab2e5e642d578
ark3127 <aman.kawale47@gmail.com>
2026-08-31 12:40:50 +0530

    Refactor main.dart by removing CystemApp widget
    Removed the CystemApp widget implementation from main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const CystemApp());
-}
-
-class CystemApp extends StatelessWidget {
-  const CystemApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CYSTEM',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'CYSTEM',
-            style: TextStyle(
-              fontSize: 32,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
 }
