c97a938f0a0f9aa4c174d1ffd25a0b1e26563bcc
ark3127 <aman.kawale47@gmail.com>
2026-09-06 20:08:51 +0530

    Refine message action controls

--- lib/widgets/message_actions.dart
+++ lib/widgets/message_actions.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../core/theme/app_theme.dart';
+
 class MessageActions extends StatelessWidget {
-  const MessageActions({
-    super.key,
-    required this.isUser,
-    required this.onCopy,
-    this.onEdit,
-    this.onRegenerate,
-    required this.onDelete,
-  });
+  const MessageActions({super.key, required this.isUser, required this.onCopy, this.onEdit, this.onRegenerate, required this.onDelete});
 
   final bool isUser;
   final VoidCallback? onCopy;
@@ -20,75 +15,26 @@ class MessageActions extends StatelessWidget {
   Widget build(BuildContext context) {
     return PopupMenuButton<_MessageAction>(
       tooltip: 'Message actions',
-      icon: const Icon(Icons.more_vert),
-      onSelected: (_MessageAction action) {
+      padding: EdgeInsets.zero,
+      icon: const Icon(Icons.more_horiz_rounded, size: 19, color: AppTheme.textMuted),
+      onSelected: (action) {
         switch (action) {
-          case _MessageAction.copy:
-            onCopy?.call();
-            break;
-          case _MessageAction.edit:
-            onEdit?.call();
-            break;
-          case _MessageAction.regenerate:
-            onRegenerate?.call();
-            break;
-          case _MessageAction.delete:
-            onDelete();
-            break;
+          case _MessageAction.copy: onCopy?.call(); break;
+          case _MessageAction.edit: onEdit?.call(); break;
+          case _MessageAction.regenerate: onRegenerate?.call(); break;
+          case _MessageAction.delete: onDelete(); break;
         }
       },
-      itemBuilder: (context) {
-        final actions = <PopupMenuEntry<_MessageAction>>[
-          const PopupMenuItem(
-            value: _MessageAction.copy,
-            child: ListTile(
-              leading: Icon(Icons.copy_outlined),
-              title: Text('Copy'),
-            ),
-          ),
-        ];
-
-        if (isUser) {
-          actions.add(
-            const PopupMenuItem(
-              value: _MessageAction.edit,
-              child: ListTile(
-                leading: Icon(Icons.edit_outlined),
-                title: Text('Edit'),
-              ),
-            ),
-          );
-        } else {
-          actions.add(
-            const PopupMenuItem(
-              value: _MessageAction.regenerate,
-              child: ListTile(
-                leading: Icon(Icons.refresh),
-                title: Text('Regenerate'),
-              ),
-            ),
-          );
-        }
-
-        actions.add(
-          const PopupMenuItem(
-            value: _MessageAction.delete,
-            child: ListTile(
-              leading: Icon(Icons.delete_outline),
-              title: Text('Delete'),
-            ),
-          ),
-        );
-
+      itemBuilder: (_) {
+        final actions = <PopupMenuEntry<_MessageAction>>[];
+        if (onCopy != null) actions.add(const PopupMenuItem(value: _MessageAction.copy, child: Text('Copy')));
+        if (isUser && onEdit != null) actions.add(const PopupMenuItem(value: _MessageAction.edit, child: Text('Edit')));
+        if (!isUser && onRegenerate != null) actions.add(const PopupMenuItem(value: _MessageAction.regenerate, child: Text('Regenerate')));
+        actions.add(const PopupMenuItem(value: _MessageAction.delete, child: Text('Delete')));
         return actions;
       },
     );
   }
 }
 
-enum _MessageAction {
-  copy,
-  edit,
-  regenerate,
-  delete,
-}
+enum _MessageAction { copy, edit, regenerate, delete }
