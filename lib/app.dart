f2d2c13d11b651a1b2fa2c414c21347d6739e19c
ark3127 <aman.kawale47@gmail.com>
2026-09-06 21:26:26 +0530

    Connect theme settings to the app

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
 
 import 'core/theme/app_theme.dart';
+import 'core/theme/app_theme_controller.dart';
 import 'screens/chat_screen.dart';
 
-class CystemApp extends StatelessWidget {
+class CystemApp extends StatefulWidget {
   const CystemApp({super.key});
+
+  @override
+  State<CystemApp> createState() => _CystemAppState();
+}
+
+class _CystemAppState extends State<CystemApp> {
+  final _themeController = AppThemeController.instance;
+
+  @override
+  void initState() {
+    super.initState();
+    _themeController.addListener(_onThemeChanged);
+    _themeController.load();
+  }
+
+  void _onThemeChanged() {
+    if (mounted) setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _themeController.removeListener(_onThemeChanged);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'CYSTEM',
       debugShowCheckedModeBanner: false,
-      theme: AppTheme.darkTheme,
+      theme: AppTheme.lightTheme(accent: _themeController.accentColor),
+      darkTheme: AppTheme.darkTheme(accent: _themeController.accentColor),
+      themeMode: _themeController.themeMode,
       home: const ChatScreen(),
     );
   }
