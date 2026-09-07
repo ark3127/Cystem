41900be2a31495d1f8ec0e2a39d65efaffefdc38
ark3127 <aman.kawale47@gmail.com>
2026-09-07 10:23:10 +0530

    Remove legacy web-search credential storage

--- lib/services/secure_storage_service.dart
+++ lib/services/secure_storage_service.dart
@@ -2,7 +2,6 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class SecureStorageService {
   static const _apiKeyKey = 'nvidia_api_key';
-  static const _tavilySearchApiKey = 'tavily_search_api_key';
   static const _geminiApiKey = 'gemini_api_key';
 
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
@@ -21,23 +20,6 @@ class SecureStorageService {
 
   Future<bool> hasApiKey() async {
     final apiKey = await getApiKey();
-    return apiKey != null && apiKey.isNotEmpty;
-  }
-
-  Future<void> saveTavilySearchApiKey(String apiKey) async {
-    await _storage.write(key: _tavilySearchApiKey, value: apiKey);
-  }
-
-  Future<String?> getTavilySearchApiKey() async {
-    return _storage.read(key: _tavilySearchApiKey);
-  }
-
-  Future<void> deleteTavilySearchApiKey() async {
-    await _storage.delete(key: _tavilySearchApiKey);
-  }
-
-  Future<bool> hasTavilySearchApiKey() async {
-    final apiKey = await getTavilySearchApiKey();
     return apiKey != null && apiKey.isNotEmpty;
   }
 
